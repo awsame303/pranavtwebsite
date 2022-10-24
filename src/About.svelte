66c0ddcad37857b20c992d993d0e3e0312ce6dcd
awsame303 <72770606+awsame303@users.noreply.github.com>
2022-10-24 18:06:53 -0400

    changed about lol

---
+++ src/About.svelte
@@ -0,0 +1,164 @@
+<main class="post">
+  <body>
+    <div class="container">
+      <div class="content">
+        <main class="post">
+          <div class="post-info">
+            <p />
+          </div>
+          <article>
+            <h2 class="post-title">
+              <a href="https://pranavt.netlify.app/about">About Me</a>
+            </h2>
+            <div class="post-content">
+              <p>Hi there,</p>
+              <p>
+                I'm a person who goes to WWMS and doesn't have a life. I spend my days programming useless stuff and overall being
+                a dumb person. All of my code is open source, so feel free to fork and contribute to it. I'm usually open to implementations.
+              </p>
+            </div>
+          </article>
+          <hr />
+          <div class="post-info" />
+        </main>
+      </div>
+      <footer class="footer">
+        <div class="footer__inner">
+          <div class="footer__content">
+            <span>© 2021</span>
+            <span
+              ><a href="https://pranavt.netlify.app/about">Pranav Thota</a
+              ></span
+            >
+            <span
+              ><a
+                href="https://creativecommons.org/licenses/by-nc/4.0/"
+                target="_blank"
+                rel="noopener">CC BY-NC 4.0</a
+              ></span
+            >
+            <span
+              ><a
+                href="https://pranavt.netlify.app/posts/index.xml"
+                target="_blank"
+                title="rss"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  stroke="currentcolor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-rss"
+                  ><path d="M4 11a9 9 0 019 9" /><path
+                    d="M4 4a16 16 0 0116 16"
+                  /><circle cx="5" cy="19" r="1" /></svg
+                ></a
+              ></span
+            >
+          </div>
+        </div>
+        <div class="footer__inner">
+          <div class="footer__content">
+            <span
+              >Powered by <a href="http://gohugo.io">Hugo</a> and
+              <a href="https://github.com/rhazdon">Djordje Atlialp</a></span
+            >
+          </div>
+        </div>
+      </footer>
+    </div>
+    <script
+      type="text/javascript"
+      src="https://irenekurien.github.io/bundle.min.599099f1f14b78b657d524b28e10e0c5098e7cd46e9c7aed73d577068a276c3ff1bb234cbf29cb313333e83cf411727b43157c91ce5b809e2ffc81664614608e.js"
+      integrity="sha512-WZCZ8fFLeLZX1SSyjhDgxQmOfNRunHrtc9V3BoonbD/xuyNMvynLMTMz6Dz0EXJ7QxV8kc5bgJ4v/IFmRhRgjg=="></script>
+  </body>
+</main>
+
+<style>
+  main {
+    background-color: rgb(35, 36, 37);
+    color: rgb(169, 169, 179);
+    margin: 0px;
+    padding: 0px;
+    font-family: "Inter UI", -apple-system, BlinkMacSystemFont, "Roboto", "Segoe UI", Helvetica, Arial, sans-serif;
+    font-size: 1rem;
+    line-height: 1.54;
+    text-rendering: optimizelegibility;
+    font-feature-settings: "liga", "tnum", "case", "calt", "zero", "ss01", "locl";
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+}
+
+.post-info {
+    margin-top: 30px;
+    font-size: 0.8rem;
+    line-height: normal;
+    opacity: 0.6;
+}
+
+.post-title {
+    font-size: 2.625rem;
+    margin: 0px 0px 20px;
+}
+
+.post-content {
+    margin-top: 30px;
+}
+
+.post-title a {
+    text-decoration: none;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 auto;
+    align-items: center;
+    justify-content: center;
+    margin: 0px;
+}
+
+.container {
+    text-align: center;
+}
+
+.post {
+    width: 100%;
+    max-width: 800px;
+    text-align: left;
+    padding: 20px;
+    margin: 20px auto;
+}
+
+main {
+    display: block;
+}
+
+.footer {
+		padding: 40px 20px;
+		flex-grow: 0;
+		color: rgb(153, 153, 153);
+    position: absolute;
+    bottom: 0;
+	}
+	.footer__inner {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 0px auto;
+		width: 760px;
+		max-width: 100%;
+	}
+	.footer__content {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		font-size: 1rem;
+		color: rgb(153, 153, 153);
+	}
+</style>
